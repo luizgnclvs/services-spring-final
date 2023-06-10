e2ad942c5adb68869552fdafea763586b2659ba3
luizgnclvs <luizgnclvss@gmail.com>
2023-06-10 17:05:21 -0300

    Deletado o DeletionResponse.java

--- src/main/java/com/unicap/aos/springfinal/domain/dto/DeletionResponse.java
+++
@@ -1,9 +0,0 @@
-package com.unicap.aos.springfinal.domain.dto;
-
-public class DeletionResponse {
-    private String message;
-
-    public DeletionResponse(String message) {
-        this.message = message;
-    }
-}
