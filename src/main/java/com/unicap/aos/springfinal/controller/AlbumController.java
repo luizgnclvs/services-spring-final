38ceea63cd3a6b2fe668e4b068796c67ef35c655
luizgnclvs <luizgnclvss@gmail.com>
2023-06-10 16:44:04 -0300

    Correção de nomes em AlbumController

--- src/main/java/com/unicap/aos/springfinal/controller/AlbumController.java
+++ src/main/java/com/unicap/aos/springfinal/controller/AlbumController.java
@@ -3,9 +3,9 @@ package com.unicap.aos.springfinal.controller;
 import com.unicap.aos.springfinal.domain.dto.AlbumCreateRequest;
 import com.unicap.aos.springfinal.domain.dto.AlbumResponse;
 import com.unicap.aos.springfinal.domain.dto.AlbumUpdateRequest;
-import com.unicap.aos.springfinal.domain.dto.DeletionResponse;
 import com.unicap.aos.springfinal.service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,7 +16,7 @@ import java.util.List;
 @RequestMapping(value = "api/album")
 public class AlbumController {
     @Autowired
-    AlbumService albumService;
+    private AlbumService albumService;
 
     @GetMapping
     public ResponseEntity<List<AlbumResponse>> getAlbums() {
@@ -24,17 +24,17 @@ public class AlbumController {
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<AlbumResponse> getAlbumById(@PathVariable int id) {
+    public ResponseEntity<AlbumResponse> getAlbumById(@PathVariable long id) {
         return ResponseEntity.ok(albumService.searchById(id));
     }
 
     @PostMapping
-    public ResponseEntity<AlbumResponse> postAlbum(@RequestBody @Valid AlbumCreateRequest request) {
-        return ResponseEntity.ok(albumService.create(request));
+    public ResponseEntity<AlbumResponse> createAlbum(@RequestBody @Valid AlbumCreateRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(albumService.create(request));
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<AlbumResponse> putAlbum(@PathVariable int id, @RequestBody AlbumUpdateRequest request) {
+    public ResponseEntity<AlbumResponse> updateAlbum(@PathVariable long id, @RequestBody AlbumUpdateRequest request) {
         return ResponseEntity.ok(albumService.update(id, request));
     }
 
