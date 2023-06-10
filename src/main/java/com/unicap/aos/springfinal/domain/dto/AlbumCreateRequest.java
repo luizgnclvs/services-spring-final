a24b8d57e653b385e489db30e4ecddebe9361796
luizgnclvs <luizgnclvss@gmail.com>
2023-06-10 16:52:12 -0300

    Add anotações JsonProperty nos DTOs

--- src/main/java/com/unicap/aos/springfinal/domain/dto/AlbumCreateRequest.java
+++ src/main/java/com/unicap/aos/springfinal/domain/dto/AlbumCreateRequest.java
@@ -1,5 +1,6 @@
 package com.unicap.aos.springfinal.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
@@ -14,8 +15,10 @@ public class AlbumCreateRequest {
     private String artist;
 
     @NotBlank(message = "URL da capa não pode ser nula, vazia ou em branco.")
+    @JsonProperty("cover_url")
     private String coverURL;
 
     @NotNull(message = "Ano de lançamento não pode ser nulo.")
+    @JsonProperty("release_year")
     private int releaseYear;
 }
