c8a4391dd0f4cefcd31ec18bb41aff7fcab3b259
luizgnclvs <luizgnclvss@gmail.com>
2023-06-10 17:06:10 -0300

    Removidas custom messages

--- src/main/java/com/unicap/aos/springfinal/domain/dto/AlbumCreateRequest.java
+++ src/main/java/com/unicap/aos/springfinal/domain/dto/AlbumCreateRequest.java
@@ -8,17 +8,17 @@ import javax.validation.constraints.NotNull;
 
 @Data
 public class AlbumCreateRequest {
-    @NotBlank(message = "Nome não pode ser nulo, vazio ou em branco.")
+    @NotBlank
     private String name;
 
-    @NotBlank(message = "Artista não pode ser nulo, vazio ou em branco.")
+    @NotBlank
     private String artist;
 
-    @NotBlank(message = "URL da capa não pode ser nula, vazia ou em branco.")
+    @NotBlank
     @JsonProperty("cover_url")
     private String coverURL;
 
-    @NotNull(message = "Ano de lançamento não pode ser nulo.")
+    @NotNull
     @JsonProperty("release_year")
     private int releaseYear;
 }
